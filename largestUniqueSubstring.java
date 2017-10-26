eac72ecdb902355a324891124e2d4b9c1eab9384
Swapnil Shah <swapnilshahmail@gmail.com>
2017-10-26 17:58:01 -0400

    adding largestUniqueSubstring

---
+++ largestUniqueSubstring.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+		public static void main(String args[]){
+			System.out.println(largestUniqueSubstring(args[0]));
+		}
+
+		public static int largestUniqueSubstring(String s){
+				if (s.length()==0) return 0;
+				HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+				int max = 0;
+				for (int i = 0, j=0; i < s.length(); i++){
+						if (map.containsKey(s.charAt(i))){
+							j = Math.max(j, map.get(s.charAt(i))+1);
+						}
+						map.put(s.charAt(i), i);
+						max = Math.max(max, i-j+1);
+				}
+				return max;
+		}
+}
